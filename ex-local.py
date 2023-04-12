02dd18c043f3fd1c74eb8beda55d6a79ed5f6da0
Akim Ignatiev <ignatiev.akim@yandex.ru>
2023-04-12 09:56:36 +0300

    delite lines

--- ex-local.py
+++ ex-local.py
@@ -2,9 +2,6 @@ print('Test')
 print('Test2')
 print('Test33')
 print('Test44')
-<<<<<<< HEAD
 print('tsdasd')
-=======
 print('test123')
 print('asddfg')
->>>>>>> f3409b255e25af8fbf6ddaad48b57767a6cf0832
